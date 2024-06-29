b6855ba0c493fb9914a0c8106873cbe609474d06
Brian Smiley <briantsmiley42@gmail.com>
2024-06-29 13:15:52 -0400

    adds a movePlayer function

--- functions.ts
+++ functions.ts
@@ -1,5 +1,22 @@
+import { Game, tileSchema } from "types";
+
 type Direction = "up" | "down" | "left" | "right";
 
 const movePlayer = (game: Game, direction: Direction): Game => {
-  return null;
+  //relative changes to playerPosition based on direction
+  //playerPosition is a location on game.grid 1D 256 array, so L and R are -/+1 and U and D are -/+16
+  const displacements = {
+    up: 16,
+    down: -16,
+    left: -1,
+    right: 1
+  };
+  //check if target tile exists and is walkable
+  const targetTileIndex = game.playerPosition + displacements[direction];
+  if (
+    game.currentLocation.grid[targetTileIndex] &&
+    game.currentLocation.grid[targetTileIndex].walkable
+  ) {
+    return { ...game, playerPosition: targetTileIndex };
+  } else return game;
 };
